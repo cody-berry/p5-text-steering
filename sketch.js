c4e517f74f3403a3a9a57debb0a12980096b8fe9
Cody <cody.tian@icloud.com>
2021-09-13 13:06:46 -0700

    Add version comments, shell

--- sketch.js
+++ sketch.js
@@ -1,4 +1,20 @@
-let font;
+/*
+version comments
+. program shell: basic setup, draw. css styling
+. load a font, use text
+  font.textToPoints » display all points
+  vehicle with: pos, vel, acc, r, maxspeed, maxforce, target
+  vehicle.show, .update,
+  vehicle.applyforce
+  vehicle.seek, flee » behaviors
+  vehicle.arrive
+  textpoints afraid of mouse
+  play with optional parameters to textToPoints
+*/
+
+
+let font
+let vehicles
 
 function preload() {
   font = loadFont('fonts/Meiryo-01.ttf');
@@ -8,17 +24,9 @@ function setup() {
   createCanvas(600, 300);
   colorMode(HSB, 360, 100, 100, 100);
 
-  textFont(font);
-  textSize(32);
-  fill(0, 0, 100);
-  noStroke();
+  let points = font.textToPoints("Trainbow", 10, height/2, 80)
 }
 
 function draw() {
   background(0, 0, 50);
-  //
-  // for(let i=0; i<100; i++) {
-  //   ellipse(random(mouseX), random(mouseY), random(50), random(50));
-  // }
-  text('[Cody Berry] Lv.72 Whm', 100, 200);
 }
