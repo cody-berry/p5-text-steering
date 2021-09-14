8addfb0d1571212c8fe93b4fec81d52c97e8fbb0
Cody Berry <cody.tian@icloud.com>
2021-09-14 12:47:58 -0700

    Add font.textToPoints

--- sketch.js
+++ sketch.js
@@ -23,10 +23,24 @@ function preload() {
 function setup() {
   createCanvas(600, 300);
   colorMode(HSB, 360, 100, 100, 100);
+  background(0, 0, 50)
+  textFont(font)
+
+  fill(210, 50, 100)
+  stroke(210, 50, 100)
+  strokeWeight(3)
+  textSize(80)
 
   let points = font.textToPoints("Trainbow", 10, height/2, 80)
+  // text("Trainbow", 10, height/2)
+
+  for (let i = 0; i < points.length; i++){
+    let pt = points[i]
+    point(pt.x, pt.y)
+  }
+
 }
 
 function draw() {
-  background(0, 0, 50);
+  // background(0, 0, 50);
 }
